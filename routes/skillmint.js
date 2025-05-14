c3a868f1c68ee5cb4dce7488d1f16bbda0c037ad
Renao Chetri <63331185+ChhetriPrem@users.noreply.github.com>
2025-05-14 13:42:08 +0530

    Update skillmint.js

--- routes/skillmint.js
+++ routes/skillmint.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const upload = multer({
+  dest: path.join('/tmp', 'uploads'),
+});
 
 const controller = require("../controllers/skillmintController");
 const mintController = require("../controllers/mintBageController");
