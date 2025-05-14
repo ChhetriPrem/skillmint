3bcdd0cd896f3a2028c643f598e9c260e2308e25
Renao Chetri <63331185+ChhetriPrem@users.noreply.github.com>
2025-05-14 13:28:27 +0530

    Update supabaseClient.js

--- services/supabaseClient.js
+++ services/supabaseClient.js
@@ -1,6 +1,6 @@
 const { createClient } = require('@supabase/supabase-js');
-require('../config'); // must be at the top
 
+require("dotenv").config();
 console.log("supabase url, ", process.env.SUPABASE_URL)
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
