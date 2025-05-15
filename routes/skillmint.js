b8f289904f455801b4f05bb5ceecc6326698123b
Renao Chetri <63331185+ChhetriPrem@users.noreply.github.com>
2025-05-15 14:20:07 +0530

    Update skillmint.js

--- routes/skillmint.js
+++ routes/skillmint.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const path = require('path');
+const path = require("path");
 const upload = multer({
-  dest: path.join('/tmp', 'uploads'),
+  dest: path.join("/tmp", "uploads"),
 });
 
 const controller = require("../controllers/skillmintController");
 const mintController = require("../controllers/mintBageController");
 
 // Create a new badge template (with image upload)
-router.post("/create-template", upload.single("file"), controller.uploadTemplateMetadata);
+router.post(
+  "/create-template",
+  upload.single("file"),
+  controller.uploadTemplateMetadata
+);
 
 // Get all badge templates
 router.get("/templates", controller.getTemplates);
@@ -20,10 +24,12 @@ router.get("/templates", controller.getTemplates);
 router.post("/mint-badge-ex", mintController.mintBadge);
 
 // Get wallet address by GitHub username
-router.get('/users/wallet', controller.getWalletByGithub);
+router.get("/users/wallet", controller.getWalletByGithub);
+router.post("/users/uploadcv", controller.uploadCVtoPianata);
+router.get("/users/publiccv", controller.publicCV);
 
 // Get all badges minted for a GitHub user
-router.get('/badges', mintController.getBadgesForWallet);
+router.get("/badges", mintController.getBadgesForWallet);
 router.post("/store-template", controller.storeTemplate);
 
 module.exports = router;
